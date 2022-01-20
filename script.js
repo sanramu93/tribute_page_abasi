4cb40f74aad242492da3dc0df017b83f3713d394
sanramu93 <sanramu93@gmail.com>
2022-01-20 12:54:14 -0500

    nav links active state fixed

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Enable hidden nav bar
 
 const nav = document.querySelector("nav");
+const navToggle = document.querySelector(".nav-toggle");
+const navBar = document.querySelector(".nav-bar");
+const anchors = document.querySelectorAll(".anchor");
+
+console.log(anchors);
 
 let lastScrollY = window.scrollY;
 
@@ -12,4 +17,14 @@ window.addEventListener("scroll", () => {
   }
 
   lastScrollY = window.scrollY;
+});
+
+navToggle.addEventListener("click", () => {
+  navBar.classList.toggle("nav-bar--active");
+});
+
+anchors.forEach((anchor) => {
+  anchor.addEventListener("click", () => {
+    navBar.classList.remove("nav-bar--active");
+  });
 });
